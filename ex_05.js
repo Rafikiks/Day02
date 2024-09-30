92bde0ac51fdf88af05fab577ac80892aab63b1b
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-09-30 12:30:46 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,8 @@
+const firstname = "Jonh"
+const lastname = "Doe"
+const cardCode = "1111 2222 3333 4444"
+const expiryDateMonth= 21
+const expiryDateyear= 2042
+const computedBackInfo= firstname+ "," +lastname+ "," +cardCode+ "," +expiryDateMonth+ "/" +expiryDateyear
+
+displayInCard(computedBackInfo)
