8759ad231713b2040e4333db78c1da361db4968a
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-09-30 10:44:06 +0200

    Create ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,6 @@
+const bool = displayThisText("true")
+const string = displayThisText("forty two")
+const integer = displayThisText("42")
+const this_is_null =console.log ("null")
+const float = displayThisText("42.42")
+
