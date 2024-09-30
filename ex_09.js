7549290a9079a77e2426a176eadd73c7af1ee20b
Rafikiks <kylian.razafitsalama@epitech.digital>
2024-09-30 15:29:44 +0200

    Update ex_09.js

--- ex_09.js
+++ ex_09.js
@@ -1,10 +1,15 @@
-displayThisText("The variable value is " +  input_var)
- if  (Number.isInteger (input_var)) {
-  displayThisText("Its type is integer")
- }else if (Number. isFinite (input_var)) {
-  displayThisText("Its type is float");
- }else {
+displayThisText("The variable value is " + input_var)
+if (Number.isInteger(input_var)) {
+	displayThisText("Its type is integer") } 
+else if (Number.isFinite(input_var)) {
+	displayThisText("Its type is float");} 
+else {
   displayThisText("Its type is " + typeof input_var)
+		}
 if (input_var == 42) {
-  displayThisText("It is the meaning of life.");
+    displayThisText("It is the meaning of life.");
 }
+
+
+
+
